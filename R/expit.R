180e01e32271d24da66470803c10a4fc2abf8a5a
Pauline Trinh <paulinetrinh@Paulines-MacBook-Pro.local>
2022-04-25 13:13:35 -0700

    change documentation of happi functions

--- R/expit.R
+++ R/expit.R
@@ -1,8 +1,15 @@
 #' expit: inverse of logit
 #'
-#' @param input vector
-#'
+#' @param input input a vector of value(s). These values can be between negative infinity to infinity.
+#' 
+#' @return Returns value(s) that are proportion(s) between 0 and 1 
 #' @export
+#' 
+#' @examples 
+#' # taking the expit of any value(s) between negative infinity and infinity & return a proportion 
+#' expit(3)
+#' # taking the expit of a vector of values 
+#' expit(c(-0.1, 2, 1.5, -1))
 expit <- function(input) {
   exp(input) / (1 + exp(input))
 }
