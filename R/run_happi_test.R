897db87b02d6c902cc0625f39a1a9d906dcb3ae9
paulinetrinh <ptrinh88@uw.edu>
2023-03-09 21:28:04 -0800

    minor changes to warning messages

--- R/run_happi_test.R
+++ R/run_happi_test.R
@@ -317,11 +317,11 @@ happi_test <- function(outcome,
         
 
         if (tail(bestOut$loglik$loglik[!is.na(bestOut$loglik$loglik)],1) < tail(bestOut_null$loglik$loglik[!is.na(bestOut_null$loglik$loglik)],1)) {
-          message("Restarting to estimate beta_alt didn't work. Penalized likelihood is still greater under the null than alt.")
+          message("Restarting to estimate beta_alt didn't work. Penalized likelihood is still greater under the null than alt. pvalue = 1")
           # message(paste("Had not converged after", tt_restart - 1, "iterations; LL % change:", round(pct_change_llks, 3)))
         }
         if (tail(bestOut$loglik$loglik_nopenalty[!is.na(bestOut$loglik$loglik_nopenalty)],1) < tail(bestOut_null$loglik$loglik_nopenalty[!is.na(bestOut_null$loglik$loglik_nopenalty)],1)) {
-          message("Weird! Nonpenalized Likelihood is also greater under the null than alt.")
+          message("Weird! Nonpenalized Likelihood is also greater under the null than alt. pvalue = 1")
         } else {
           message("Phew! Nonpenalized Likelihood is not greater under the null than alt.")
           }
