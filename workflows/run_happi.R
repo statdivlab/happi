f7128f3903855caf358ff5ccb58371b0db41c3fc
paulinetrinh <ptrinh88@uw.edu>
2023-04-19 08:58:45 -0700

    update workflow items

--- workflows/run_happi.R
+++ workflows/run_happi.R
@@ -3,22 +3,20 @@
 if (!require("argparse", quietly = TRUE))
   install.packages("argparse") # check that argparse is installed
 library(argparse)
-
 # parse data
 parser <- ArgumentParser(description= 'This is a workflow for running happi')
 parser$add_argument('--input', '-i', help= 'Input file')
 parser$add_argument('--output', '-o', help= 'Output file')
 parser$add_argument('--quality_var', '-q', help= 'Quality variable', type= 'double')
-parser$add_argument('--myFactor', '-f', help= 'Imported variable', type= 'double')
-parser$add_argument('--myFactor', '-f', help= 'Imported variable', type= 'double')
-parser$add_argument('--myFactor', '-f', help= 'Imported variable', type= 'double')
-parser$add_argument('--myFactor', '-f', help= 'Imported variable', type= 'double')
-parser$add_argument('--myFactor', '-f', help= 'Imported variable', type= 'double')
-parser$add_argument('--myFactor', '-f', help= 'Imported variable', type= 'double')
-parser$add_argument('--myFactor', '-f', help= 'Imported variable', type= 'double')
-parser$add_argument('--myFactor', '-f', help= 'Imported variable', type= 'double')
-parser$add_argument('--myFactor', '-f', help= 'Imported variable', type= 'double')
-parser$add_argument('--myFactor', '-f', help= 'Imported variable', type= 'double')
+parser$add_argument('--covariate', '-c', help= 'Imported variable', type= 'double')
+parser$add_argument('--minit', '-a', help= 'Imported variable', type= 'double')
+parser$add_argument('--maxit', '-z', help= 'Imported variable', type= 'double')
+parser$add_argument('--method', '-m', help= 'Imported variable', type= 'double')
+parser$add_argument('--firth', '-f', help= 'Imported variable', type= 'double')
+parser$add_argument('--spline_df', '-d', help= 'Imported variable', type= 'double')
+parser$add_argument('--perm', '-p', help= 'Imported variable', type= 'double')
+parser$add_argument('--nstarts', '-n', help= 'Imported variable', type= 'double')
+parser$add_argument('--seed', '-s', help= 'Imported variable', type= 'double')
 
 xargs<- parser$parse_args()
 # process data
